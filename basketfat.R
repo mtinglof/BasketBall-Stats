6525d246586a76af5ec9c587a1025f1a27c8d630
mtinglof <42756027+mtinglof@users.noreply.github.com>
2019-02-25 22:14:36 -0800

    playercreate finished/ instance of percent and points added

--- basketfat.R
+++ basketfat.R
@@ -1,29 +1,38 @@
 playercreate = function(name, cost, n){
   library("readxl")
-  playerexcel = read_excel("D:/Dev/PredBasketball/lebjam", sheet = name) 
-  fg = matrix(rnorm(n, mean=mean(log(playerexcel$FG)), sd=sd(log(playerexcel$FG))))
-  para3 = (dim(playerexcel)[1]-2)/sum(playerexcel$'3P')
-  p3 = matrix(rexp(n, para3))
-  rb = matrix(rnorm(n, mean=mean(log(playerexcel$TRB)), sd=sd(log(playerexcel$TRB))))
-  ta = matrix(rnorm(n, mean=mean(log(playerexcel$AST)), sd=sd(log(playerexcel$AST))))
-  stpara = (dim(playerexcel)[1]-2)/sum(playerexcel$STL)
-  st = matrix(rexp(n, stpara))
-  blpara = (dim(playerexcel)[1]-2)/sum(playerexcel$BLK)
-  bl = matrix(rexp(n, blpara))
-  to = matrix(rnorm(n, mean=mean(log(playerexcel$TOV)), sd=sd(log(playerexcel$TOV))))
+  playerexcel = read_excel("D:/Dev/PredBasketball/lebjam.xlsx", sheet = name) 
   
-  fgp = 1+(.5-pnorm(abs(fg-mean(log(playerexcel$FG)))/sd(log(playerexcel$FG))))*2
-  p3p = exp(-para3*abs(p3-(1/para3)))
-  rbp = 1+(.5-pnorm(abs(rb-mean(log(playerexcel$TRB)))/sd(log(playerexcel$TRB))))*2
-  tap = 1+(.5-pnorm(abs(ta-mean(log(playerexcel$AST)))/sd(log(playerexcel$AST))))*2
-  stp = exp(-stpara*abs(st-(1/stpara)))
-  blp = exp(-blpara*abs(bl-(1/blpara)))
-  to = 1+(.5-pnorm(abs(to-mean(log(playerexcel$TOV)))/sd(log(playerexcel$TOV))))*2
-  total.percent = fgp*p3p*rbp*tap*stp*blp*to
+  player.fieldgoal = playerexcel$FG + 10
+  player.rebounds = playerexcel$TRB + 10
+  player.assists = playerexcel$AST + 10 
+  player.turnover = playerexcel$TOV + 10 
   
-  player = cbind(fg, p3, rb, ta, st, bl, to, total.percent)
-  rowname(player) = name
-  colnames(player) = c("cost", "2pt", "3pt", "rebounds", "assists", "steals", "blocks", "TO", "%")
-  return(player)
+  fieldgoal = matrix(rnorm(n, mean=mean(log(player.fieldgoal)), sd=sd(log(player.fieldgoal))))
+  three.parameter = (dim(playerexcel)[1]-2)/sum(playerexcel$'3P')
+  threepoints = matrix(rexp(n, three.parameter))
+  rebounds = matrix(rnorm(n, mean=mean(log(player.rebounds)), sd=sd(log(player.rebounds))))
+  total.assists = matrix(rnorm(n, mean=mean(log(player.assists)), sd=sd(log(player.assists))))
+  steals.parameter = (dim(playerexcel)[1]-2)/sum(playerexcel$STL)
+  steals = matrix(rexp(n, steals.parameter))
+  blocks.parameter = (dim(playerexcel)[1]-2)/sum(playerexcel$BLK)
+  blocks = matrix(rexp(n, blocks.parameter))
+  turnover = matrix(rnorm(n, mean=mean(log(player.turnover)), sd=sd(log(player.turnover))))
+  
+  fieldgoal.percent = 1+(.5-pnorm(abs(fieldgoal-mean(log(player.fieldgoal)))/sd(log(player.fieldgoal))))*2
+  threepoints.percent = exp(-three.parameter*abs(threepoints-(1/three.parameter)))
+  rebounds.percent = 1+(.5-pnorm(abs(rebounds-mean(log(player.rebounds)))/sd(log(player.rebounds))))*2
+  total.assists.percent = 1+(.5-pnorm(abs(total.assists-mean(log(player.assists)))/sd(log(player.assists))))*2
+  steals.percent = exp(-steals.parameter*abs(steals-(1/steals.parameter)))
+  blocks.percent = exp(-blocks.parameter*abs(blocks-(1/blocks.parameter)))
+  turnover.percent = 1+(.5-pnorm(abs(turnover-mean(log(player.turnover)))/sd(log(player.turnover))))*2
+  total.percent = fieldgoal.percent*threepoints.percent*rebounds.percent*total.assists.percent*steals.percent*blocks.percent*turnover
+  
+  weights = matrix(c(0, 1, 1.5, 1.25, 1.5, 2, 2, -.5, 0))
+  player = cbind(cost, exp(fieldgoal)-10, threepoints, exp(rebounds)-10, exp(total.assists)-10, steals, blocks, exp(turnover)-10, total.percent)
+  score = player%*%weights
+  player =cbind(player, score)
+  
+  colnames(player) = c("cost", "2pt", "3pt", "rebounds", "assists", "steals", "blocks", "turnover", "T%", "points")
+  return(signif(player, digits = 3))
 }
 

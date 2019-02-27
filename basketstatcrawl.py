cb3d8d13fde267b9dca3fd05150e0e666942bb3a
mtinglof <42756027+mtinglof@users.noreply.github.com>
2019-02-27 11:48:10 -0800

    Webscrapper update. Cleans data during runtime

--- basketstatcrawl.py
+++ basketstatcrawl.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import html5lib
 from pandas import ExcelWriter
+import html5lib
+import numpy as np
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -29,21 +31,34 @@ class basketstatcrawl:
         [self.open(player) for player in names]
 
     def open(self, name):
-        searchbar = self.browser.find_element_by_xpath("//input[@tabindex='1']")
-        searchbar.send_keys(name[0:-1])
-        searchbar.send_keys(Keys.RETURN)
         try:
-            self.browser.find_element_by_xpath("//tr[@id='per_game.2019.clone']").click()
+            searchbar = self.browser.find_element_by_xpath("//input[@tabindex='1']")
+            searchbar.send_keys(name[0:-1])
+            searchbar.send_keys(Keys.RETURN)
+            try:
+                self.browser.find_element_by_xpath("//tr[@id='per_game.2019.clone']").click()
+            except NoSuchElementException:
+                winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+                print("Please resolve error. Probably name error?")
+                input()
+                try:
+                    self.browser.find_element_by_xpath("//tr[@id='per_game.2019.clone']").click()
+                except NoSuchElementException:
+                    maintable = self.browser.find_element_by_xpath("//div[@id='all_per_game']")
+                    tablelist = maintable.find_elements(By.TAG_NAME, "th")
+                    tablelist[-2].click()
+            table = self.browser.find_element_by_xpath("//table[@id='pgl_basic']")
+            df = pd.read_html(table.get_attribute("outerHTML"))[0]
+            df['FG'].replace('', np.nan, inplace=True)
+            df['FG'].replace('FG', np.nan, inplace=True)
+            df.dropna(subset=['FG'], inplace=True)
+            df.to_excel(self.writer, name)
+            self.writer.save()
         except NoSuchElementException:
             winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-            print("Help! I'm looking for " + name + ". Press enter when done")
+            print("Were we 404'ed?")
             input()
-            self.browser.find_element_by_xpath("//tr[@id='per_game.2019.clone']").click()
-            pass
-        table = self.browser.find_element_by_xpath("//table[@id='pgl_basic']")
-        df = pd.read_html(table.get_attribute("outerHTML"))
-        df[0].to_excel(self.writer, name)
-        self.writer.save()
+            self.open(name)
 
         
 test = basketstatcrawl('https://www.basketball-reference.com/')

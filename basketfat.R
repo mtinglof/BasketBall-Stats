c17d9b7cb505d6c87689913b482686c9a8ec6783
mtinglof <42756027+mtinglof@users.noreply.github.com>
2019-03-04 01:09:05 -0800

    updated combo creator

--- basketfat.R
+++ basketfat.R
@@ -31,15 +31,15 @@ playercreate = function(name, n){
   turnover.percent = 1+(.5-pnorm(abs(turnover-mean(log(player.turnover)))/sd(log(player.turnover))))*2
   total.percent = fieldgoal.percent*threepoints.percent*rebounds.percent*total.assists.percent*steals.percent*blocks.percent*turnover
   
-  weights = matrix(c(1, 2, 3.5, 1.25, 1.5, 2, 2, -.5, 0))
-  player = cbind(exp(freethrow)-10, exp(fieldgoal)-10, threepoints, exp(rebounds)-10, exp(total.assists)-10, steals, blocks, exp(turnover)-10, total.percent)
+  weights = matrix(c(1, 2, 3.5, 1.25, 1.5, 2, 2, -.5))
+  player = cbind(exp(freethrow)-10, exp(fieldgoal)-10, threepoints, exp(rebounds)-10, exp(total.assists)-10, steals, blocks, exp(turnover)-10)
   score = player%*%weights
   player =cbind(player, score)
-  #plot(player[,8], player[,9], xlab="Percent", ylab="Score")
-  player.dist =cbind(mean(player[,10]), sd(player[,10])) 
+  #player = cbind(player, total.percent)
+  player.dist =cbind(mean(player[,9]), sd(player[,9])) 
   player.dist = (signif(player.dist, digits = 3))
   
-  #colnames(player) = c("FT", "2pt", "3pt", "rebounds", "assists", "steals", "blocks", "turnover", "T%", "points")
+  #colnames(player) = c("FT", "2pt", "3pt", "rebounds", "assists", "steals", "blocks", "turnover", "points", "T%")
   colnames(player.dist) = c("mean", "sd")
   rownames(player.dist) = c(name)
   return(player.dist) 
@@ -57,7 +57,8 @@ playergen = function(n){
     all.player[i, 2] = player[1, 2]
     i = i + 1
   }
-  rownames(all.player) = all.data$Name
+  #rownames(all.player) = all.data$Name
+  colnames(all.player) = c("Mean", "SD")
   return(all.player)
 }
 
@@ -69,4 +70,40 @@ playercheck = function(name){
   player = cbind(playerexcel$FT, playerexcel$FG, playerexcel$'3P', playerexcel$TRB, playerexcel$AST, playerexcel$STL, playerexcel$BLK, playerexcel$TOV)
   playerpoints = player%*%weights
   return(c(mean(playerpoints), sd(playerpoints)))
+}
+
+playercombos = function(n, gensize){
+  library("readxl")
+  path = "D:/Dev/PredBasketball/BasketBall-Stats/MILvSAC-NOvLAL.xlsx"
+  all.data = read_excel(path=path, sheet="ALL")
+  
+  pg.index = which(all.data$Position %in% c("PG", "PG/SG", "PG/SF"))
+  sg.index = which(all.data$Position %in% c("PG/SG", "SG/SF"))
+  sf.index = which(all.data$Position %in% c("PG/SF", "SF/PF", "SG/SF"))
+  pf.index = which(all.data$Position %in% c("PF", "PF/C", "SF/PF"))
+  c.index = which(all.data$Position %in% c("C", "PF/C"))
+  g.index = which(all.data$Position %in% c("PG", "PG/SG", "PG/SF", "SG/SF"))
+  f.index = which(all.data$Position %in% c("PG/SF", "SG/SF", "PF", "PF/C", "SF/PF"))
+  util.index = 1:dim(all.data)[1]
+  
+  generated.data = playergen(gensize)
+  all.data = cbind(all.data, generated.data)
+  
+  highscore = 0
+  finalsheet = 0
+  j = 0 
+  index = (unique(c(sample(pg.index, n, replace = T), sample(sg.index, n, replace = T), sample(sf.index, n, replace = T), sample(pf.index, n, replace = T), sample(c.index, n, replace = T), 
+    sample(g.index, n, replace = T), sample(f.index, n, replace = T), sample(util.index, n, replace = T))))
+  index = (matrix(index, nrow = n, ncol = 8))
+  for(i in 1:n){
+    combopoints = sum(all.data$Mean[index[i,]]*(all.data$SD[index[i,]]*2))
+    combocost = sum(all.data$Salary[index[i,]])
+    if((combopoints > highscore) & (combocost < 50001)){
+      finalteam = t(all.data$Name[index[i,]])
+      finalteam = cbind(finalteam, combopoints)
+      finalsheet[j] = finalteam
+      j = j + 1
+    }
+  }
+  return(finalsheet)
 }
